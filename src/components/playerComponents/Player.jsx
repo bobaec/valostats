63b20c7f69ba8afedad313411c42dab3fb194f10
Gascon1 <mikelgascon@outlook.com>
2020-05-03 19:30:14 -0400

    create rank component

--- src/components/playerComponents/Player.jsx
+++ src/components/playerComponents/Player.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Player.scss';
 import PlayerHeader from './PlayerHeader';
+import Rank from './Rank';
 
 export default function Player() {
     return (
         <div className='player-wrapper'>
             <div className='player-content'>
                 <PlayerHeader />
-                <div className='default-container'>
-                    <img id='rank-icon' src='rankPlaceholder.jpg' alt='' />
-                </div>
+                <Rank />
             </div>
         </div>
     );
--- src/components/playerComponents/PlayerHeader.jsx
+++ src/components/playerComponents/PlayerHeader.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function PlayerHeader() {
     return (
-        <div className="default-container">
-            <img className="player-icon" src='portraitPlaceholder.png' alt="" />
-            <div className="player-info-container">
-                <span className="player-name">Mikel</span>
-                <button className="refresh-button">Refresh</button>
+        <div id='player-header' className='default-container'>
+            <img className='player-icon' src='portraitPlaceholder.png' alt='' />
+            <div className='player-info-container'>
+                <span className='player-name'>Mikel</span>
+                <button className='refresh-button'>Refresh</button>
             </div>
         </div>
     );
