08242ad588999e49d3b31ae68495a813e2a847a9
Gascon1 <mikelgascon@outlook.com>
2020-05-07 19:55:40 -0400

    refactor graph container

--- src/components/playerComponents/HistoryOverview.jsx
+++ src/components/playerComponents/HistoryOverview.jsx
@@ -11,6 +11,7 @@ export default function Rank() {
           <li>Unrated</li>
         </ul>
         {/* <div className='seperator'></div> */}
+        {/* this should be an input with autocomplete */}
         <div className='agent-searchbox'>
           <span className='agent-text'>Search an Agent</span>
           <i class='fas fa-search'></i>
@@ -18,28 +19,15 @@ export default function Rank() {
       </div>
 
       <div className='default-container content-container overview-container'>
-        <div className='flex-row'>
-          <table>
-            <tr>
-              <span>20G</span>
-              <span> </span>
-              <span>12W</span>
-              <span> </span>
-              <span>7L</span>
-              <span> </span>
-            </tr>
-            <tr>
-              <i class='fas fa-chart-pie fa-6x'></i>
-            </tr>
-          </table>
-          <table id='table2'>
-            <tr>
-              <span>5.3 / 3.5 / 10.0</span>
-            </tr>
-            <tr>
-              <span>4.36:1 (61%)</span>
-            </tr>
-          </table>
+        <div>
+          <div className='graph-stats-container'>
+            <i class='fas fa-chart-pie fa-6x pie-chart'></i>
+            <div className='stats-container'>
+              <span className='stats'>20G / 12W / 8L</span>
+              <span className='stats'>5.3 / 3.5 / 10.0</span>
+              <span className='stats'>4.36:1 (61%)</span>
+            </div>
+          </div>
         </div>
         <div className='seperator'></div>
         <div className='flex-row'>
