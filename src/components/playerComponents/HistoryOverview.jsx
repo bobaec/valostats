9702b25a444765e4daa385bc2dc9469180d40717
Gascon1 <mikelgascon@outlook.com>
2020-05-07 19:23:09 -0400

    modify new overview header

--- src/components/playerComponents/HistoryOverview.jsx
+++ src/components/playerComponents/HistoryOverview.jsx
@@ -1,78 +1,73 @@
-import React from "react";
-import "./HistoryOverview.scss";
+import React from 'react';
+import './HistoryOverview.scss';
 
 export default function Rank() {
   return (
-    <React.Fragment>
-      <div className="history-overview-navigation">
+    <>
+      <div className='default-container header-container history-overview-navigation'>
         <ul>
-          <li>Total</li>
-          <li>Ranked Solo</li>
-          <li>Ranked Flex</li>
-          <li className="selected">Queue Type</li>
+          <li className='selected'>Total</li>
+          <li>Competitive</li>
+          <li>Unrated</li>
         </ul>
-
-
-        <div className="agent-searchbox">
-        <div className="seperator"></div>
-          <div>
-            Search an Agent <i class="fas fa-search"></i>
-          </div>
+        {/* <div className='seperator'></div> */}
+        <div className='agent-searchbox'>
+          <span className='agent-text'>Search an Agent</span>
+          <i class='fas fa-search'></i>
         </div>
       </div>
 
-      <div className="overview-game-average-stat-box">
-        <div className="box">
-          <div className="flex-row">
-            <table>
-              <tr>
-                <span>20G</span><span> </span>
-                <span>12W</span><span> </span>
-                <span>7L</span><span> </span>
-              </tr>
-              <tr>
-                <i class="fas fa-chart-pie fa-6x"></i>
-              </tr>
-            </table>
-            <table id="table2">
-              <tr>
-                <span>5.3 / 3.5 / 10.0</span>
-              </tr>
-              <tr>
-                <span>4.36:1 (61%)</span>
-              </tr>
-            </table>
-          </div>
+      <div className='default-container content-container overview-container'>
+        <div className='flex-row'>
+          <table>
+            <tr>
+              <span>20G</span>
+              <span> </span>
+              <span>12W</span>
+              <span> </span>
+              <span>7L</span>
+              <span> </span>
+            </tr>
+            <tr>
+              <i class='fas fa-chart-pie fa-6x'></i>
+            </tr>
+          </table>
+          <table id='table2'>
+            <tr>
+              <span>5.3 / 3.5 / 10.0</span>
+            </tr>
+            <tr>
+              <span>4.36:1 (61%)</span>
+            </tr>
+          </table>
+        </div>
+        <div className='seperator'></div>
+        <div className='flex-row'>
+          <table>
+            <tr>
+              <td>icon </td>
+              <td>info </td>
+              <td>info </td>
+            </tr>
+            <tr></tr>
+            <tr></tr>
+          </table>
+        </div>
 
-          <div className="seperator"></div>
+        <div className='seperator'></div>
 
-          <div className="flex-row">
-            <table>
-              <tr>
-                <td>icon </td>
-                <td>info </td>
-                <td>info </td>
-              </tr>
-              <tr></tr>
-              <tr></tr>
-            </table>
-          </div>
-
-          <div className="seperator"></div>
-
-          <div className="flex-row">
-            <table>
-              <tr>
-                <td>icon </td>
-                <td>info </td>
-                <td>info </td>
-              </tr>
-              <tr></tr>
-              <tr></tr>
-            </table>
-          </div>
+        <div className='flex-row'>
+          <table>
+            <tr>
+              <td>icon </td>
+              <td>info </td>
+              <td>info </td>
+            </tr>
+            <tr></tr>
+            <tr></tr>
+          </table>
         </div>
       </div>
-    </React.Fragment>
+    </>
   );
 }
