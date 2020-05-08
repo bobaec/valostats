9c4fe5045a2383ba89c95e969b7924388fe6c846
Gascon1 <mikelgascon@outlook.com>
2020-05-08 19:09:49 -0400

    fix searchbox styling

--- src/components/playerComponents/HistoryOverviewSearchbox.jsx
+++ src/components/playerComponents/HistoryOverviewSearchbox.jsx
@@ -1,50 +1,37 @@
-import React, { useState } from "react";
-import "./HistoryOverviewSearchbox.scss";
+import React, { useState } from 'react';
+import './HistoryOverviewSearchbox.scss';
 
 export default function HistoryOverviewSearchBox() {
   const [state, setState] = useState({
     showDropdown: false,
-    filter: "",
+    filter: '',
   });
 
-  let characterList = [
-    "Breach",
-    "Brimstone",
-    "Cypher",
-    "Jett",
-    "Omen",
-    "Phoenix",
-    "Sage",
-    "Sova",
-    "Viper",
-  ];
+  let characterList = ['Breach', 'Brimstone', 'Cypher', 'Jett', 'Omen', 'Phoenix', 'Sage', 'Sova', 'Viper'];
 
   let dropdownList =
-    state.filter === ""
+    state.filter === ''
       ? characterList.map((characterName) => <li>{characterName}</li>)
       : characterList
-          .filter((characterName) =>
-            characterName.toLowerCase().includes(state.filter.toLowerCase())
-          )
+          .filter((characterName) => characterName.toLowerCase().includes(state.filter.toLowerCase()))
           .map((filteredCharacterName) => <li>{filteredCharacterName}</li>);
 
   return (
     <>
-      <div className="history-overview-searchbox">
-        <div className="searchbox-input">
+      <div className='history-overview-searchbox'>
+        <div className='searchbox-input'>
           <input
-            type="text"
-            class="Input"
-            placeholder="Search an Agent"
+            type='text'
+            class='input'
+            placeholder='Search an Agent'
             onFocus={(e) => setState({ ...state, showDropdown: true })}
             onBlur={(e) => setState({ ...state, showDropdown: false })}
-            onChange={(e) => setState({ ...state, filter: e.target.value })}
-          ></input>
-          <i className="fas fa-search"></i>
+            onChange={(e) => setState({ ...state, filter: e.target.value })}></input>
+          <i className='fas fa-search'></i>
         </div>
-        <div className="searchbox-dropdown-menu">
+        <div className='searchbox-dropdown-menu'>
           {state.showDropdown && (
-            <ul id="searchbox-dropdown-agent-list">
+            <ul id='searchbox-dropdown-agent-list'>
               <li>All Agents</li>
               {dropdownList}
             </ul>
@@ -53,7 +40,4 @@ export default function HistoryOverviewSearchBox() {
       </div>
     </>
   );
-}
-{
-  /* this should be an input with autocomplete */
 }
