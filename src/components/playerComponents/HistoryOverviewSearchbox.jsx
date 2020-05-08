2791f7e1c4104c9c29a637441c534bd372f23f9e
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-05-08 16:11:03 -0400

    add filtering behaviour to searchbox

--- src/components/playerComponents/HistoryOverviewSearchbox.jsx
+++ src/components/playerComponents/HistoryOverviewSearchbox.jsx
@@ -4,7 +4,29 @@ import "./HistoryOverviewSearchbox.scss";
 export default function HistoryOverviewSearchBox() {
   const [state, setState] = useState({
     showDropdown: false,
+    filter: "",
   });
+
+  let characterList = [
+    "Breach",
+    "Brimstone",
+    "Cypher",
+    "Jett",
+    "Omen",
+    "Phoenix",
+    "Sage",
+    "Sova",
+    "Viper",
+  ];
+
+  let dropdownList =
+    state.filter === ""
+      ? characterList.map((characterName) => <li>{characterName}</li>)
+      : characterList
+          .filter((characterName) =>
+            characterName.toLowerCase().startsWith(state.filter.toLowerCase())
+          )
+          .map((filteredCharacterName) => <li>{filteredCharacterName}</li>);
 
   return (
     <>
@@ -16,22 +38,15 @@ export default function HistoryOverviewSearchBox() {
             placeholder="Search an Agent"
             onFocus={(e) => setState({ ...state, showDropdown: true })}
             onBlur={(e) => setState({ ...state, showDropdown: false })}
+            onChange={(e) => setState({ ...state, filter: e.target.value })}
           ></input>
-          <i className="fas fa-search" ></i>
+          <i className="fas fa-search"></i>
         </div>
         <div className="searchbox-dropdown-menu">
           {state.showDropdown && (
             <ul id="searchbox-dropdown-agent-list">
               <li>All Agents</li>
-              <li>Breach</li>
-              <li>Brimstone</li>
-              <li>Cypher</li>
-              <li>Jett</li>
-              <li>Omen</li>
-              <li>Phoenix</li>
-              <li>Sage</li>
-              <li>Sova</li>
-              <li>Viper</li>
+              {dropdownList}
             </ul>
           )}
         </div>
