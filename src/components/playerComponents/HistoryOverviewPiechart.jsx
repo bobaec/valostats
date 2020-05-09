b449d4dcd64aa1465f6e154681cb06876a7c154a
Gascon1 <mikelgascon@outlook.com>
2020-05-09 15:43:38 -0400

    fix labels on piechart

--- src/components/playerComponents/HistoryOverviewPiechart.jsx
+++ src/components/playerComponents/HistoryOverviewPiechart.jsx
@@ -1,33 +1,38 @@
-import React, { useState } from "react";
-import { PieChart } from "react-minimal-pie-chart";
+import React, { useState } from 'react';
+import { PieChart } from 'react-minimal-pie-chart';
 
 export default function HistoryOverviewPiechart() {
+  const style = {
+    fill: 'white',
+    fontSize: '7px',
+  };
+
   return (
     <PieChart
       animationDuration={500}
-      animationEasing="ease-out"
+      animationEasing='ease-out'
       center={[50, 50]}
       data={[
         {
-          color: "#68ba63",
-          title: "One",
+          color: '#68ba63',
+          title: 'One',
           value: 65,
         },
         {
-          color: "#df5e59",
-          title: "Two",
+          color: '#df5e59',
+          title: 'Two',
           value: 35,
         },
       ]}
       label={({ dataEntry }) => dataEntry.value}
+      labelPosition={125}
+      labelStyle={style}
       totalValue={100}
-      rounded={false}
-      center={[50, 50]}
-      labelPosition={10}
+      rounded={true}
       lengthAngle={360}
       lineWidth={20}
-      paddingAngle={2}
-      radius={30}
+      paddingAngle={20}
+      radius={25}
       startAngle={150}
       viewBoxSize={[100, 100]}
     />
