6b66584a3b13a263296a8f62c15fab79352ee3be
Gascon1 <mikelgascon@outlook.com>
2020-05-09 16:32:57 -0400

    color text on chart according to chart color

--- src/components/playerComponents/HistoryOverviewPiechart.jsx
+++ src/components/playerComponents/HistoryOverviewPiechart.jsx
@@ -2,14 +2,25 @@ import React, { useState } from 'react';
 import { PieChart } from 'react-minimal-pie-chart';
 
 export default function HistoryOverviewPiechart() {
-  const style = {
-    fill: 'white',
-    fontSize: '7px',
+  const style = (segmentIndex) => {
+    if (!segmentIndex) {
+      return {
+        fill: '#68ba63',
+        fontSize: '9px',
+        fontWeight: '600',
+      };
+    }
+    return {
+      fill: '#df5e59',
+      fontSize: '9px',
+      fontWeight: '600',
+    };
   };
 
   return (
     <PieChart
       animationDuration={500}
+      animate
       animationEasing='ease-out'
       center={[50, 50]}
       data={[
@@ -26,15 +37,16 @@ export default function HistoryOverviewPiechart() {
       ]}
       label={({ dataEntry }) => dataEntry.value}
       labelPosition={125}
-      labelStyle={style}
+      labelStyle={(segmentIndex) => style(segmentIndex)}
       totalValue={100}
       rounded={true}
       lengthAngle={360}
       lineWidth={20}
       paddingAngle={20}
-      radius={25}
-      startAngle={150}
+      radius={30}
+      startAngle={135}
       viewBoxSize={[100, 100]}
+      style={{ width: '60%' }}
     />
   );
 }
