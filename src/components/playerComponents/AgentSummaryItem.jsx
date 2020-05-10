d96f5e379d18be91f36f64b0412030f41e7db001
Gascon1 <mikelgascon@outlook.com>
2020-05-09 20:09:33 -0400

    make agent summaries into a component

---
+++ src/components/playerComponents/AgentSummaryItem.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function AgentSummaryItem() {
+  return (
+    <>
+      <div className='summary-item'>
+        <img className='hero-icon' src='portraitPlaceholder.png' alt='' />
+        <div className='summary-agent-name'>Brimstone</div>
+        <div className='summary-kda-container'>
+          <div className='kda-summary'>3.82:1 KDA</div>
+          <div className='kda-breakdown'>6.3 / 3.4 / 6.7</div>
+        </div>
+        <div className='summary-kda-container'>
+          <div className='winrate-summary'>65%</div>
+          <div className='winrate-breakdown'>20 Played</div>
+        </div>
+      </div>
+      <div className='seperator horizontal' />
+    </>
+  );
+}
--- src/components/playerComponents/SummaryByAgent.jsx
+++ src/components/playerComponents/SummaryByAgent.jsx
@@ -1,48 +1,16 @@
 import React from 'react';
+import AgentSummaryItem from './AgentSummaryItem';
 
 export default function SummaryByAgent() {
   return (
     <div className='summary-wrapper'>
       <div className='default-container header-container'>Summary</div>
       <div className='default-container content-container summary-container'>
-        <div className='summary-item'>
-          <img className='hero-icon' src='portraitPlaceholder.png' alt='' />
-          <div className='summary-agent-name'>Brimstone</div>
-          <div className='summary-kda-container'>
-            <div className='kda-summary'>3.82:1 KDA</div>
-            <div className='kda-breakdown'>6.3 / 3.4 / 6.7</div>
-          </div>
-          <div className='summary-kda-container'>
-            <div className='winrate-summary'>65%</div>
-            <div className='winrate-breakdown'>20 Played</div>
-          </div>
-        </div>
-        <div className='seperator horizontal' />
-        <div className='summary-item'>
-          <img className='hero-icon' src='portraitPlaceholder.png' alt='' />
-          <div className='summary-agent-name'>Brimstone</div>
-          <div className='summary-kda-container'>
-            <div className='kda-summary'>3.82:1 KDA</div>
-            <div className='kda-breakdown'>6.3 / 3.4 / 6.7</div>
-          </div>
-          <div className='summary-kda-container'>
-            <div className='winrate-summary'>65%</div>
-            <div className='winrate-breakdown'>20 Played</div>
-          </div>
-        </div>
-        <div className='seperator horizontal' />
-        <div className='summary-item'>
-          <img className='hero-icon' src='portraitPlaceholder.png' alt='' />
-          <div className='summary-agent-name'>Brimstone</div>
-          <div className='summary-kda-container'>
-            <div className='kda-summary'>3.82:1 KDA</div>
-            <div className='kda-breakdown'>6.3 / 3.4 / 6.7</div>
-          </div>
-          <div className='summary-kda-container'>
-            <div className='winrate-summary'>65%</div>
-            <div className='winrate-breakdown'>20 Played</div>
-          </div>
-        </div>
+        <AgentSummaryItem />
+        <AgentSummaryItem />
+        <AgentSummaryItem />
+        <AgentSummaryItem />
+        <AgentSummaryItem />
       </div>
     </div>
   );
