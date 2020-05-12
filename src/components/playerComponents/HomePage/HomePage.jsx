0577413580a4163b1306aba65d68d6edb5c9a3da
Gascon1 <mikelgascon@outlook.com>
2020-05-11 20:39:36 -0400

    change background styling so it doesnt overlap with nav

--- src/components/playerComponents/HomePage/HomePage.jsx
+++ src/components/playerComponents/HomePage/HomePage.jsx
@@ -1,16 +1,12 @@
-import React from "react";
-import Navbar from "./Navbar"
+import React from 'react';
+import Navbar from './Navbar';
 
-import "./HomePage.scss";
+import './HomePage.scss';
 
 export default function HomePage() {
-
   return (
     <>
-      <div className="background-image-container">
-      </div>
-
-    
+      <img src='tempBackground.jpg' alt='' id='bg' />
     </>
-  )
+  );
 }
