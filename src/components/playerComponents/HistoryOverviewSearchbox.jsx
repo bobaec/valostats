d82f9b4e7fc6ebb015ea0e619eec1a01459084ae
Gascon1 <mikelgascon@outlook.com>
2020-05-13 21:26:58 -0400

    fix dropdown to be a fixed size

--- src/components/playerComponents/HistoryOverviewSearchbox.jsx
+++ src/components/playerComponents/HistoryOverviewSearchbox.jsx
@@ -7,13 +7,25 @@ export default function HistoryOverviewSearchBox() {
     filter: '',
   });
 
-  let characterList = ['Breach', 'Brimstone', 'Cypher', 'Jett', 'Omen', 'Phoenix', 'Sage', 'Sova', 'Viper'];
+  let characterList = [
+    'Breach',
+    'Brimstone',
+    'Cypher',
+    'Jett',
+    'Omen',
+    'Phoenix',
+    'Sage',
+    'Sova',
+    'Viper',
+  ];
 
   let dropdownList =
     state.filter === ''
       ? characterList.map((characterName) => <li>{characterName}</li>)
       : characterList
-          .filter((characterName) => characterName.toLowerCase().includes(state.filter.toLowerCase()))
+          .filter((characterName) =>
+            characterName.toLowerCase().includes(state.filter.toLowerCase())
+          )
           .map((filteredCharacterName) => <li>{filteredCharacterName}</li>);
 
   return (
@@ -26,16 +38,18 @@ export default function HistoryOverviewSearchBox() {
             placeholder='Search an Agent'
             onFocus={(e) => setState({ ...state, showDropdown: true })}
             onBlur={(e) => setState({ ...state, showDropdown: false })}
-            onChange={(e) => setState({ ...state, filter: e.target.value })}></input>
+            onChange={(e) =>
+              setState({ ...state, filter: e.target.value })
+            }></input>
           <i className='fas fa-search'></i>
         </div>
         <div className='searchbox-dropdown-menu'>
-          {state.showDropdown && (
-            <ul id='searchbox-dropdown-agent-list'>
-              <li>All Agents</li>
-              {dropdownList}
-            </ul>
-          )}
+          {/* {state.showDropdown && ( */}
+          <ul id='searchbox-dropdown-agent-list'>
+            <li>All Agents</li>
+            {dropdownList}
+          </ul>
+          {/* )} */}
         </div>
       </div>
     </>
