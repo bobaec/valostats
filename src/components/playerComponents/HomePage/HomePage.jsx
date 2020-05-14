590b117de8f1c414842ce3513bf0d40d69baa15d
Gascon1 <mikelgascon@outlook.com>
2020-05-13 21:18:41 -0400

    change navbar anatomy

--- src/components/playerComponents/HomePage/HomePage.jsx
+++ src/components/playerComponents/HomePage/HomePage.jsx
@@ -11,49 +11,50 @@ export default function HomePage() {
   const history = useHistory();
 
   const addToLocalStorage = function (username) {
-
-    const historyArr = JSON.parse(window.localStorage.getItem("history")) ?
-      JSON.parse(window.localStorage.getItem("history")) : []
+    const historyArr = JSON.parse(window.localStorage.getItem('history'))
+      ? JSON.parse(window.localStorage.getItem('history'))
+      : [];
 
     if (historyArr.length > 9) {
-      historyArr.shift()
-      historyArr.push(username)
-      window.localStorage.setItem("history", JSON.stringify(historyArr))
+      historyArr.shift();
+      historyArr.push(username);
+      window.localStorage.setItem('history', JSON.stringify(historyArr));
     } else {
-      historyArr.push(username)
-      window.localStorage.setItem("history", JSON.stringify(historyArr))
+      historyArr.push(username);
+      window.localStorage.setItem('history', JSON.stringify(historyArr));
     }
-  }
+  };
 
-  const historyList = JSON.parse(window.localStorage.getItem("history")) ?
-    [...new Set(JSON.parse(window.localStorage.getItem("history")))] : []
+  const historyList = JSON.parse(window.localStorage.getItem('history'))
+    ? [...new Set(JSON.parse(window.localStorage.getItem('history')))]
+    : [];
 
   return (
     <>
-      <div className='homepage'>
-        <img src={background} alt='' id='bg' />
+      <img src={background} alt='' id='bg' />
 
-        <form
-          className='centered'
-          onSubmit={(e) => {
-            e.preventDefault();
-            history.push({
-              pathname: `/player/username=${state.username}`,
-            });
-            addToLocalStorage(state.username);
-          }}>
-          <input
-            type="text"
-            placeholder="Player Name"
-            onChange={(e) => setState({ ...state, username: e.target.value })}
-            list="searchbox-dropdown-history-list"
-          />
+      <form
+        className='centered'
+        onSubmit={(e) => {
+          e.preventDefault();
+          history.push({
+            pathname: `/player/username=${state.username}`,
+          });
+          addToLocalStorage(state.username);
+        }}>
+        <input
+          type='text'
+          placeholder='Player Name'
+          onChange={(e) => setState({ ...state, username: e.target.value })}
+          list='searchbox-dropdown-history-list'
+        />
 
-          <datalist id='searchbox-dropdown-history-list'>
-            {historyList.map(historyElement => <option value={historyElement} />)}
-          </datalist>
-        </form>
-      </div>
+        <datalist id='searchbox-dropdown-history-list'>
+          {historyList.map((historyElement) => (
+            <option value={historyElement} />
+          ))}
+        </datalist>
+      </form>
     </>
   );
 }
--- src/components/playerComponents/HomePage/Navbar.jsx
+++ src/components/playerComponents/HomePage/Navbar.jsx
@@ -1,70 +1,65 @@
-import React, { useState } from "react";
-import "./Navbar.scss";
-import { Link, useHistory } from 'react-router-dom'
+import React, { useState } from 'react';
+import './Navbar.scss';
+import { Link, useHistory } from 'react-router-dom';
 
 export default function Navbar(props) {
-
   const [state, setState] = useState({
-    username: "",
-  })
+    username: '',
+  });
 
   const history = useHistory();
 
   const addToLocalStorage = function (username) {
-
-    const historyArr = JSON.parse(window.localStorage.getItem("history")) ?
-      JSON.parse(window.localStorage.getItem("history")) : []
+    const historyArr = JSON.parse(window.localStorage.getItem('history'))
+      ? JSON.parse(window.localStorage.getItem('history'))
+      : [];
 
     if (historyArr.length > 9) {
-      historyArr.shift()
-      historyArr.push(username)
-      window.localStorage.setItem("history", JSON.stringify(historyArr))
+      historyArr.shift();
+      historyArr.push(username);
+      window.localStorage.setItem('history', JSON.stringify(historyArr));
     } else {
-      historyArr.push(username)
-      window.localStorage.setItem("history", JSON.stringify(historyArr))
+      historyArr.push(username);
+      window.localStorage.setItem('history', JSON.stringify(historyArr));
     }
-  }
+  };
 
-  const historyList = JSON.parse(window.localStorage.getItem("history")) ?
-    [...new Set(JSON.parse(window.localStorage.getItem("history")))] : []
-
+  const historyList = JSON.parse(window.localStorage.getItem('history'))
+    ? [...new Set(JSON.parse(window.localStorage.getItem('history')))]
+    : [];
 
   return (
     <>
-      <div className='navbar-container'>
-        <ul>
-          <li>
-            <Link to="/homepage">Home</Link>
-          </li>
-          {props.allowSearch &&
-            <li>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault()
-                  history.push(`/player/username=${state.username}`)
-                  addToLocalStorage(state.username)
-                    e.target.firstElementChild.blur()
-                    e.target.firstElementChild.value = ""
+      <nav className='navbar-container'>
+        <div className='navbar-inner-container'>
+          <Link to='/'>Home</Link>
+          {props.allowSearch && (
+            <form
+              onSubmit={(e) => {
+                e.preventDefault();
+                history.push(`/player/username=${state.username}`);
+                addToLocalStorage(state.username);
+                e.target.firstElementChild.blur();
+                e.target.firstElementChild.value = '';
+              }}>
+              <input
+                type='text'
+                placeholder='Player Name'
+                onChange={(e) =>
+                  setState({ ...state, username: e.target.value })
+                }
+                list='searchbox-dropdown-history-list'
+              />
 
-                }}
-              >
-                <input
-                  type="text"
-                  placeholder="Player Name"
-                  onChange={(e) => setState({ ...state, username: e.target.value })}
-                  list="searchbox-dropdown-history-list"
-                />
-
-                <datalist id='searchbox-dropdown-history-list'>
-                  {historyList.map(historyElement => <option value={historyElement} />)}
-                </datalist>
-
-              </form>
-            </li>}
-
-        </ul>
-
-      </div>
+              <datalist id='searchbox-dropdown-history-list'>
+                {historyList.map((historyElement) => (
+                  <option value={historyElement} />
+                ))}
+              </datalist>
+            </form>
+          )}
+        </div>
+      </nav>
     </>
-  )
+  );
 }
