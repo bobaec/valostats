65df33615e394a81ae0c9bf6e4480a5e777a3d47
Bobae <bobaec@sfu.ca>
2020-05-18 17:44:34 -0700

    redeclare var as let, rename lowercase function name for clarity

--- src/Components/HomePage/SearchBox/HomePageSearchBox.jsx
+++ src/Components/HomePage/SearchBox/HomePageSearchBox.jsx
@@ -11,13 +11,13 @@ export default function HomePageSearchBox(props) {
 
   const addToLocalStorage = function (username) {
 
-    var historyArr = JSON.parse(window.localStorage.getItem('history'))
+    let historyArr = JSON.parse(window.localStorage.getItem('history'))
       ? JSON.parse(window.localStorage.getItem('history'))
       : [];
 
     let caseInsensitive = username.toLowerCase();
 
-    historyArr = historyArr.map(function (x) { return x.toLowerCase() })
+    historyArr = historyArr.map(function (lowerCaseHistoryArr) { return lowerCaseHistoryArr.toLowerCase() })
 
     if (!historyArr.includes(caseInsensitive)) {
       if (historyArr.length > 9) {
@@ -32,8 +32,8 @@ export default function HomePageSearchBox(props) {
   };
 
   function deleteTarget(target) {
-    var arr = JSON.parse(localStorage.getItem("history"));
-    var index = arr.indexOf(target);
+    const arr = JSON.parse(localStorage.getItem("history"));
+    const index = arr.indexOf(target);
     if (index !== -1) arr.splice(index, 1);
     window.localStorage.setItem('history', JSON.stringify(arr));
   }
