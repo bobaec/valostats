0f9186238aec3c8ef9a53096d94f2d4d958d471b
Gascon1 <mikelgascon@outlook.com>
2020-05-20 00:19:37 -0400

    fix typo

--- src/components/TempHome.js
+++ src/components/TempHome.js
@@ -12,7 +12,7 @@ export default function TempHome() {
                     <header className="announcement-header">
                         Valostats will be online asap
                     </header>
-                    <span className="announcement-text">Valostats will be the home for VALORANT statistics. We want to design a tool that will provide players with their match history, live game stats, their player profile, and various stats depending on what the api provides. User experience is at the top of our priority list, we want to make this website as easy and as pleasant to use as possible. The website will be up and running as soon as the VALORANT api is ready.We are stoked and can't wait to become part of the VALORANT community!</span>
+                    <span className="announcement-text">Valostats will be the home for VALORANT statistics. We want to design a tool that will provide players with their match history, live game stats, their player profile, and various stats depending on what the api provides. User experience is at the top of our priority list, we want to make this website as easy and as pleasant to use as possible. The website will be up and running as soon as the VALORANT api is ready. We are stoked and can't wait to become part of the VALORANT community!</span>
                 </div>
             </div>
         </>
