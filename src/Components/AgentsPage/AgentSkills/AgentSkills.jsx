6cd588444413eb71b418d5aaed936251053cfe65
Bobae <bobaec@sfu.ca>
2020-05-21 22:56:58 -0700

    add respective keybind to selected skill with css

--- src/Components/AgentsPage/AgentSkills/AgentSkills.jsx
+++ src/Components/AgentsPage/AgentSkills/AgentSkills.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './AgentSkills.scss';
 import { ReactComponent as Money } from 'Images/money.svg';
+
 export default function AgentSkills(props) {
   return (
     <div className='skill-description-container'>
-      <div className='skill-name'>{props.skillName}</div>
+      <div className='skill-name'>
+        {props.skillName}
+        {<span className='hotkey'>{props.activeSkill}</span>}
+      </div>
       <div className='skill-description-wrapper'>
         <div className='skill-description'>{props.skillDescription}</div>
         <div className='skill-cost'>
