5bc04c99d8941a5e0bf078689eb72e0d1fe2d172
Bobae <bobaec@sfu.ca>
2020-05-22 16:41:39 -0700

    reset default to breach's c skill

--- src/Components/AgentsPage/AgentsPage.jsx
+++ src/Components/AgentsPage/AgentsPage.jsx
@@ -8,8 +8,8 @@ import usePrevious from 'Hooks/usePrevious';
 
 export default function AgentsPage(props) {
   const [state, setState] = useState({
-    agentName: 'Viper',
-    activeSkill: 'q',
+    agentName: 'Breach',
+    activeSkill: 'c',
     agent: {},
   });
 
