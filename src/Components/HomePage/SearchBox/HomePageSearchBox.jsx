9456478a3cec0d3b30f610139052497cca3a4d9d
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-05-22 14:59:42 -0400

    make placeholder message and color change if username is not valid, combine helper functions related to local storage into 1 file

--- src/Components/HomePage/SearchBox/HomePageSearchBox.jsx
+++ src/Components/HomePage/SearchBox/HomePageSearchBox.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import './HomePageSearchBox.scss';
-import { addToLocalStorage, deleteTarget } from 'Helpers/LocalStorageManipulation';
-// import { deleteTarget } from 'Helpers/deleteTarget';
+import {
+  addToLocalStorage,
+  deleteTarget,
+} from 'Helpers/localStorageManipulation';
 import { validateUsername } from 'Helpers/validateUsername';
 
 export default function HomePageSearchBox() {
@@ -20,7 +22,10 @@ export default function HomePageSearchBox() {
   return (
     <div>
       <div className='player-searchbox-container'>
-        <div className={`player-searchbox ${historyList.length === 0 && 'searchbox-rounded'}`}>
+        <div
+          className={`player-searchbox ${
+            historyList.length === 0 && 'searchbox-rounded'
+          }`}>
           <form
             className='searchbox-form'
             onSubmit={(e) => {
@@ -44,7 +49,9 @@ export default function HomePageSearchBox() {
             }}>
             <input
               type='text'
-              className={`player-search-input ${state.invalidUsername && `invalid-username`}`}
+              className={`player-search-input ${
+                state.invalidUsername && `invalid-username`
+              }`}
               placeholder={state.inputPlaceholder}
               onChange={(e) =>
                 setState({
@@ -74,7 +81,9 @@ export default function HomePageSearchBox() {
                   }}>
                   {searchElement}
                 </li>
-                <i class='fas fa-times' onClick={() => deleteTarget(searchElement)}></i>
+                <i
+                  class='fas fa-times'
+                  onClick={() => deleteTarget(searchElement)}></i>
               </div>
             ))}
           </ul>
