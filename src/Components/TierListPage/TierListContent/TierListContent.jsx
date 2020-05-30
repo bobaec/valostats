f10f8d1afdeab0a1385eb2abdb152c10d5f5da83
Gascon1 <mikelgascon@outlook.com>
2020-05-30 19:20:30 -0400

    center agents properly on the table

--- src/Components/TierListPage/TierListContent/TierListContent.jsx
+++ src/Components/TierListPage/TierListContent/TierListContent.jsx
@@ -9,8 +9,10 @@ export default function TierListContent(props) {
       <td>{props.rank}</td>
       <td>{agentType.type}</td>
       <td>
-        <img className={'tier-list-agent-icon'} src={img} alt='' />
-        {props.agent}
+        <td>
+          <img className={'tier-list-agent-icon'} src={img} alt='' />
+        </td>
+        <td>{props.agent}</td>
       </td>
       <td>{props.tier}</td>
       <td>{props.winRate}</td>
