4d7aeade9d83ad173cc275491c22d0cd84e69be2
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2020-06-02 13:22:55 -0400

    switch react snap to react snapshot

--- src/index.js
+++ src/index.js
@@ -1,19 +1,12 @@
 import React from 'react';
-import { hydrate, render } from "react-dom";
-import { loadComponents, getState } from "loadable-components";
+import { render } from 'react-snapshot';
 import 'index.css';
 import 'Styles/main.scss';
 import App from './App.jsx';
 
-window.snapSaveState = () => getState();
-
-const rootElement = document.getElementById("root");
-
-loadComponents()
-  .then(() => hydrate(<App />, rootElement))
-  .catch(() => render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>, rootElement));
-
-
+render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
